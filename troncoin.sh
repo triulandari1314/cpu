86d72c4afea2df1dc92865ae188a1e8b9e6a3b46
triulandari1314 <99237614+triulandari1314@users.noreply.github.com>
2022-02-08 16:32:16 +0700

    Update troncoin.sh

--- troncoin.sh
+++ troncoin.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz 
-tar -xf xmrig-6.14.0-linux-x64.tar.gz 
-cd xmrig-6.14.0
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
+tar -xf xmrig-6.16.4-linux-x64.tar.gz
+cd xmrig-6.16.4-linux-x64
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.jbgithub -p x --cpu4
 while [ 1 ]; do
 sleep 3
