5ccbd2c0ebeccbc992b16c088a5ec093de6c2772
triulandari1314 <99237614+triulandari1314@users.noreply.github.com>
2022-02-08 16:09:21 +0700

    Update troncoin.sh

--- troncoin.sh
+++ troncoin.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
-tar -xf lolMiner_v1.29_Lin64.tar.gz 
-cd 1.29 
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.jbgithub --ethstratum ETHPROXY
+wget https://github.com/xmrig/xmrig/releases/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz 
+tar -xf xmrig-6.14.0-linux-x64.tar.gz 
+cd xmrig-6.14.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.jbgithub -p x --cpu4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
 
