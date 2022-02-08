69f13497b8d72518e2d2ef2f619decedecc57277
triulandari1314 <99237614+triulandari1314@users.noreply.github.com>
2022-02-08 15:49:36 +0700

    Create troncoin.sh

---
+++ troncoin.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
+tar -xf lolMiner_v1.29_Lin64.tar.gz 
+cd 1.29 
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.jbgithub --ethstratum ETHPROXY
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
